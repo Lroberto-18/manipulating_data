cb18e26d73e632c18e44ceae06d60c7881cda2ab
Lroberto-18 <leandroroberto582@gmail.com>
2022-11-23 20:15:36 -0300

    atualizações

--- data_organizer.py
+++ data_organizer.py
@@ -8,7 +8,9 @@ data_rooms = ['Sala 01','Sala 02', 'Sala 03',
 data_names = ['Carlos Vinicius','Maria Santos','Marta Coelho','Ricardo Nobre',
               'Julho Almeida','Breno Aguiar','Alex Menezes','Manuela Mendes', 
               'Jefferson Cardoso','Marcos Silva', 'Roberto Nunes']
-
+df_in = {'Rooms':data_rooms, 'Team':data_names}
+d_outIn = pd.DataFrame(df_in)
+display(d_outIn)
 ids = np.arange(1,len(data_rooms))
 #contruir data Frame
 df = {'ID':ids}
@@ -16,9 +18,8 @@ dataFrame = pd.DataFrame(df)
 w1 = set(data_rooms) #remover repetidos
 for i in range(len(w1)):
   dataFrame.insert(0, list(w1)[i],'')
-
+  
 dataFrame.drop('ID',axis=1, inplace=True) #remover coluna id
-
 #distribuir os dados
 for x in range(dataFrame.shape[1]):
   z=0
@@ -37,4 +38,4 @@ for y in range(dataFrame.shape[0]):
       control+=1
   if(control==4):
     dataFrame.drop(y, inplace=True)
-dataFrame
+display(dataFrame)
